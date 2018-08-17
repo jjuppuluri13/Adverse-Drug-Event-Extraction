c9375ced4e3741ccca6182052848a81b8132b189
jjuppuluri13 <41755561+jjuppuluri13@users.noreply.github.com>
2018-08-17 11:46:30 -0700

    Create data_proc.py

---
+++ data_cleanup.md/data_proc.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+This is a temporary script file.
+"""
+import psycopg2 as pg
+import pandas.io.sql as psql
+import pandas as pd
+from sqlalchemy import create_engine
+conn = pg.connect("dbname=AE_Positive user=postgres password=Mypostgresql#1a")
+# read postgres table into pandas dataframe
+df = pd.read_sql_query('SELECT * FROM pos_table', conn)
+
+
+## remove punctuation from sentences & drugs, replace with spaces
+import string
+translator = str.maketrans(string.punctuation, ' '*len(string.punctuation))
+df.sentence = df.sentence.apply(lambda x: x.translate(translator))
+df.drug = df.drug.apply(lambda x: x.translate(translator))
+
+
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+stop_words = set(stopwords.words('english'))
+
+## remove stopwords from sentences
+def remove_stop (sent):
+    word_tokens = word_tokenize(sent)
+    sent2 = [w for w in word_tokens if not w in stop_words]
+    sent2 = ' '.join(ch for ch in sent2)
+    return sent2
+
+df.sentence.apply(remove_stop)
+df.sentence = df.sentence.apply(remove_stop)
+df.drug = df.drug.apply(remove_stop)
+
+
+## Stemming & Lemmatization
+from nltk import pos_tag
+from nltk.stem import WordNetLemmatizer
+wnl = WordNetLemmatizer()
+# Lemmatization Function
+def lemma(sent):
+    words = word_tokenize(sent)
+    out_sen = [wnl.lemmatize(i,j[0].lower()) if j[0].lower() in ['a','n','v'] else wnl.lemmatize(i) for i,j in pos_tag(words)]
+    out_sen = ' '.join(ch for ch in out_sen)
+    return out_sen
+
+from nltk.stem.porter import PorterStemmer
+ps = PorterStemmer()
+
+# Stemming Function
+def word_stem(tokens):
+    stems = []
+    for w in tokens:
+        stems.append(ps.stem(w))
+    return stems
+
+def pstem(sent):
+    tokens = word_tokenize(sent)
+    fin_sen = word_stem(tokens)
+    #fin_sen = ' '.join(ch for ch in fin_sen)
+    return fin_sen
+
+def pos_label(tokens):
+    labels = nltk.pos_tag(tokens)
+    return labels
+
+
+#df.sentence = df.sentence.apply(lambda x: lemma(x))
+#df.drug = df.drug.apply(lambda x: lemma(x))
